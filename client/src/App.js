4f4f9fc0d6cbac17a1d38fc650521663f2324fac
Jacob D. Castro <jdcastro.business@gmail.com>
2019-10-06 20:01:32 -0700

    new reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './components/pages/Landing';
 import CreateGame from './components/pages/CreateGame';
 import JoinGame from './components/pages/JoinGame';
+import CreatePlayer from './components/pages/CreatePlayer';
+
 import AppWrapper from './styles/App.sty.js';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 import store from './redux/store';
 import { Provider } from 'react-redux';
@@ -17,13 +21,16 @@ const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <AppWrapper>
-          <Switch>
-            <Route exact path="/create-game" component={CreateGame} />
-            <Route exact path="/join-game" component={JoinGame} />
-            <Route exact path="/" component={Landing} />
-          </Switch>
-        </AppWrapper>
+        <ThemeProvider theme={theme}>
+          <AppWrapper>
+            <Switch>
+              <Route exact path="/create-game" component={CreateGame} />
+              <Route exact path="/join-game" component={JoinGame} />
+              <Route exact path="/create-player" component={CreatePlayer} />
+              <Route exact path="/" component={Landing} />
+            </Switch>
+          </AppWrapper>
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   );
