aa80a723533984a438341f56042b7f9ee5414e59
Jacob D. Castro <jdcastro.business@gmail.com>
2019-10-15 05:38:38 -0700

    fix bug that returned getCurrentRoundData undefined

--- client/src/components/pages/Lobby.js
+++ client/src/components/pages/Lobby.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getGameData } from '../../redux/actions/game';
 import { getPlayerData } from '../../redux/actions/player';
-import { getCurrentRoundData } from '../../redux/actions/currentRound';
 
 import LobbyWrapper from '../../styles/lobby/Lobby.sty.js';
 import Standings from '../lobby/Standings';
@@ -15,10 +14,8 @@ const Lobby = ({
   isGamemaster,
   isLoading,
   game,
-  currentRound,
   getGameData,
   getPlayerData,
-  getCurrentRoundData,
 }) => {
   const [pageView, setPageView] = useState(0); // 0 = standings, 1 = rounds
   const { players, rounds } = game;
@@ -27,18 +24,16 @@ const Lobby = ({
     getGameData(localStorage.gameId);
   }, []);
 
-  const updateLowerState = () => {
-    if (players) {
-      const playerData = players.find(p => p._id === localStorage.playerId);
-      getPlayerData(playerData);
-    }
-    if (rounds) {
-      const currentRoundData = rounds.find(r => r._id === game.currentRound);
-      getCurrentRoundData(currentRoundData);
-    }
+  const updatePlayerState = () => {
+    const playerData = players.find(p => p._id === localStorage.playerId);
+    getPlayerData(playerData);
   };
 
-  // if (players && rounds) updateLowerState();
+  let currentRoundData;
+  if (players && rounds) {
+    updatePlayerState();
+    currentRoundData = rounds.find(r => r._id === game.currentRound);
+  }
 
   let pageViewComponent;
   // wait for rounds/players to load before rendering page view component
@@ -76,13 +71,13 @@ const Lobby = ({
       <button
         onClick={() => {
           getGameData(localStorage.gameId);
-          updateLowerState();
+          updatePlayerState();
         }}
       >
         Update Game State
       </button>
 
-      {/* {!currentRound.inProgress && currentRound.finished && <SubmitScore />} */}
+      {currentRoundData && <SubmitScore roundData={currentRoundData} />}
 
       {rounds && players && isGamemaster && <GMFooter />}
     </LobbyWrapper>
@@ -92,21 +87,18 @@ const Lobby = ({
 Lobby.propTypes = {
   isLoading: PropTypes.bool,
   game: PropTypes.object,
-  currentRound: PropTypes.object,
   isGamemaster: PropTypes.bool,
   getGameData: PropTypes.func.isRequired,
   getPlayerData: PropTypes.func.isRequired,
-  getCurrentRoundData: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   isLoading: state.game.isLoading,
   game: state.game,
-  currentRound: state.currentRound,
   isGamemaster: state.player.isGamemaster,
 });
 
 export default connect(
   mapStateToProps,
-  { getGameData, getPlayerData, getCurrentRoundData }
+  { getGameData, getPlayerData }
 )(Lobby);
