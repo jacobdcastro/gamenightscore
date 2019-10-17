03f8162d59ad7bfc7909faa07dedd590ca17deda
Jacob D. Castro <jdcastro.business@gmail.com>
2019-10-16 21:26:53 -0700

    move header logic to own component

---
+++ client/src/components/lobby/CurrentRoundHeader.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const CurrentRoundHeader = ({ roundData, players }) => {
+  const { inProgress, finished, allScoresSubmitted, newRoundReady } = roundData;
+  let currentRoundWinner;
+  currentRoundWinner = players.find(p => p._id === roundData.winner);
+  console.log(currentRoundWinner);
+
+  return (
+    <div>
+      <h2>Current Round: {roundData ? roundData.roundNumber : '...'}</h2>
+
+      {!inProgress && !finished && (
+        <p>
+          New Round! <br />
+          Waiting on Gamemaster to start round...
+        </p>
+      )}
+
+      {inProgress && !finished && <p>Round has begun!!!</p>}
+
+      {!inProgress && finished && (
+        <p>
+          Round is over!
+          <br />
+          {roundData.winner && `${currentRoundWinner.name} has won.`}
+          <br />
+          Please enter your score below.
+        </p>
+      )}
+      {newRoundReady && allScoresSubmitted && (
+        <p>
+          All scores have updated! Waiting on Gamemaster to go to next round...
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default CurrentRoundHeader;
