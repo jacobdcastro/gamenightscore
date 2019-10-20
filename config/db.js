03b724ae268a68c7fabf967db24458134f0d1911
Jacob D. Castro <jdcastro.business@gmail.com>
2019-10-19 18:32:28 -0700

    add config to MongoDB to use new Server Discovery and Monitoring engine

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useFindAndModify: false,
+      useUnifiedTopology: true,
     });
 
     console.log('MongoDB Connected!');
