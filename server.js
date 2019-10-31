ade5e24b435c61e85ea3fbf5a730b059828d11c8
Jacob D. Castro <jdcastro.business@gmail.com>
2019-10-31 11:52:34 -0700

    remove config from express.json()

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const pusher = new Pusher({
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
