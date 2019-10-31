237b8d8d92cf6923df2baf3f2f3f7716bf731ed4
Jacob D. Castro <jdcastro.business@gmail.com>
2019-10-30 18:19:00 -0700

    server react files from /client

--- server.js
+++ server.js
@@ -37,8 +37,11 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
+app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('/', (req, res) => res.send('GameNight Score API Running Properly!'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
 
 // Define routes
 app.use('/api/games', require('./routes/api/games'));
