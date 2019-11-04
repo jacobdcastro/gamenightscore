c6c7f0bc65fa62f22b4a9f086784687cf8a0724f
Jacob D. Castro <jdcastro.business@gmail.com>
2019-11-03 20:13:21 -0800

    rename files

---
+++ client/src/components/lobby/PlayerSubmitScore.js
@@ -0,0 +1,71 @@
+import React, { useState, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { submitPlayerScore } from '../../redux/actions/game';
+import ScoreForm from './ScoreForm';
+import {
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+  Button,
+} from '@material-ui/core';
+
+const PlayerSubmitScore = ({
+  roundData,
+  playerId,
+  gameId,
+  submitPlayerScore,
+}) => {
+  const [roundScore, setRoundScore] = useState(0);
+  const { roundNumber } = roundData;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const actionData = { gameId, playerId, roundScore };
+    submitPlayerScore(actionData);
+  };
+
+  return (
+    <Fragment>
+      <DialogTitle>
+        {playerId === roundData.winner
+          ? `Congrats! You won round ${roundNumber}! Enter your score.`
+          : `Enter score for round ${roundNumber}`}
+      </DialogTitle>
+
+      <DialogContent>
+        <ScoreForm roundScore={roundScore} setRoundScore={setRoundScore} />
+      </DialogContent>
+
+      <DialogActions>
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          className="submitScoreBtn"
+          type="submit"
+          onClick={e => handleSubmit(e)}
+        >
+          Submit Score
+        </Button>
+      </DialogActions>
+    </Fragment>
+  );
+};
+
+PlayerSubmitScore.propTypes = {
+  roundData: PropTypes.object,
+  playerId: PropTypes.string.isRequired,
+  gameId: PropTypes.string.isRequired,
+  submitPlayerScore: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  playerId: state.player._id,
+  gameId: state.game._id,
+});
+
+export default connect(
+  mapStateToProps,
+  { submitPlayerScore }
+)(PlayerSubmitScore);
---
+++ client/src/components/lobby/gamemaster/GMSubmitScores.js
@@ -0,0 +1,113 @@
+import React, { useState, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { submitPlayerScore } from '../../../redux/actions/game';
+import ScoreForm from '../ScoreForm';
+import {
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+  Button,
+} from '@material-ui/core';
+
+const GMSubmitScores = ({
+  gmPlayerId,
+  players,
+  currentRoundIsScored,
+  currentRoundData,
+  allGmPlayersScoresSubmitted,
+  submitPlayerScore,
+}) => {
+  const [roundScore, setRoundScore] = useState(0);
+  const [playerBeingScored, setPlayerBeingScored] = useState();
+  const [gmIsScored, setGmIsScored] = useState(false);
+  let [index, setIndex] = useState(0);
+
+  let gmCreatedPlayers = players.filter(p => p.gmCreated === true);
+
+  // object is added to/manipulated in handle_()'s
+  let actionData = {
+    gameId: localStorage.gameId,
+  };
+
+  // TODO fix dumb score lifecycle breaking
+
+  const submitGmScore = () => {
+    actionData.playerId = gmPlayerId;
+    actionData.roundScore = roundScore;
+    submitPlayerScore(actionData);
+    setGmIsScored(true);
+    setPlayerBeingScored(gmCreatedPlayers[index]._id);
+  };
+
+  const submitCreatedPlayerScore = () => {
+    actionData.playerId = playerBeingScored;
+    actionData.roundScore = roundScore;
+    submitPlayerScore(actionData);
+
+    if (index + 1 < gmCreatedPlayers.length) {
+      setIndex((index += 1));
+      setPlayerBeingScored(gmCreatedPlayers[index]._id);
+    }
+  };
+
+  const handleScoreSubmit = () => {
+    if (gmIsScored) {
+      submitCreatedPlayerScore();
+    } else {
+      submitGmScore();
+    }
+
+    setRoundScore(0);
+
+    // reset state
+    if (allGmPlayersScoresSubmitted) {
+      setGmIsScored(false);
+      setIndex(0);
+    }
+  };
+
+  return (
+    <Fragment>
+      <DialogTitle>
+        {gmIsScored && index < gmCreatedPlayers.length
+          ? `Enter round ${currentRoundData.roundNumber} score for ${gmCreatedPlayers[index].name}.`
+          : `Enter your score for round ${currentRoundData.roundNumber}`}
+      </DialogTitle>
+
+      <DialogContent>
+        <ScoreForm roundScore={roundScore} setRoundScore={setRoundScore} />
+      </DialogContent>
+
+      <DialogActions>
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          onClick={() => handleScoreSubmit()}
+        >
+          Submit Score
+        </Button>
+      </DialogActions>
+    </Fragment>
+  );
+};
+
+GMSubmitScores.propTypes = {
+  gmPlayerId: PropTypes.string.isRequired,
+  players: PropTypes.array.isRequired,
+  currentRoundIsScored: PropTypes.bool.isRequired,
+  allGmPlayersScoresSubmitted: PropTypes.bool.isRequired,
+  currentRoundData: PropTypes.object,
+  submitPlayerScore: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  players: state.game.players,
+  gmPlayerId: state.player._id,
+});
+
+export default connect(
+  mapStateToProps,
+  { submitPlayerScore }
+)(GMSubmitScores);
