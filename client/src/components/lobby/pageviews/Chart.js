84dc05aa79b2849357b8f213e6683417a172de94
Jacob D. Castro <jdcastro.business@gmail.com>
2019-11-09 17:09:42 -0800

    begin full development

--- client/src/components/lobby/pageviews/Chart.js
+++ client/src/components/lobby/pageviews/Chart.js
@@ -10,19 +10,21 @@ import {
 } from 'react-vis';
 import 'react-vis/dist/style.css';
 
-const Chart = ({ players, rounds, maxNumberOfRounds }) => {
-  const data = [{ x: 1, y: 10 }, { x: 6, y: 5 }, { x: 3, y: 15 }];
-  const data1 = [{ x: 2, y: 13 }, { x: 7, y: 2 }, { x: 3, y: 8 }];
-  const data2 = [{ x: 9, y: 6 }, { x: 11, y: 4 }, { x: 6, y: 4 }];
+// TODO umm just finish this
+//  https://uber.github.io/react-vis/documentation/series-reference/line-series
 
+const Chart = ({ players, rounds, maxNumberOfRounds }) => {
   return (
     <Paper>
       <h1>Stats</h1>
       <XYPlot width={300} height={300}>
         <HorizontalGridLines />
-        <LineSeries color="red" strokeWidth={4} data={data} />
-        <LineSeries color="blue" strokeWidth={4} data={data1} />
-        <LineSeries color="green" strokeWidth={4} data={data2} />
+        {players.map((p, index) => {
+          const data = [{ x: 1, y: 10 }, { x: 6, y: 5 }, { x: 3, y: 15 }];
+
+          return <LineSeries color="red" strokeWidth={4} data={data} />;
+        })}
+
         <XAxis title="round" />
         <YAxis title="score" />
       </XYPlot>
