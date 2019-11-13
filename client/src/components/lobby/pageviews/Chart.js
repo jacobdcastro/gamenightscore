c87cd922eed23f134eb6cb590fd069b252f1671c
Jacob D. Castro <jdcastro.business@gmail.com>
2019-11-12 17:29:03 -0800

    begin adding more chart functionality

--- client/src/components/lobby/pageviews/Chart.js
+++ client/src/components/lobby/pageviews/Chart.js
@@ -1,33 +1,65 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Paper } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { Paper } from "@material-ui/core";
 import {
   XYPlot,
   XAxis,
   YAxis,
   HorizontalGridLines,
-  LineSeries,
-} from 'react-vis';
-import 'react-vis/dist/style.css';
+  LineSeries
+} from "react-vis";
+import "react-vis/dist/style.css";
 
 // TODO umm just finish this
 //  https://uber.github.io/react-vis/documentation/series-reference/line-series
 
+const lineColors = [
+  "red",
+  "blue",
+  "yellow",
+  "#800080", // purple
+  "green",
+  "#add8e6", // lightblue
+  "black",
+  "#ffc0cb" // pink
+];
+
 const Chart = ({ players, rounds, maxNumberOfRounds }) => {
+  players.sort((a, b) => a._id - b._id); // sort by id to prevent reordering
+  const [playerScores, setPlayerScores] = useState({
+    players: [],
+    scores: []
+  });
+
+  useEffect(() => {
+    console.log("Chart updated");
+  }, [players]);
+
   return (
     <Paper>
       <h1>Stats</h1>
       <XYPlot width={300} height={300}>
         <HorizontalGridLines />
         {players.map((p, index) => {
-          const data = [{ x: 1, y: 10 }, { x: 6, y: 5 }, { x: 3, y: 15 }];
-
-          return <LineSeries color="red" strokeWidth={4} data={data} />;
+          let data = [{ x: 0, y: 0 }]; // set 'round 0'
+          p.roundsPlayed.forEach(r =>
+            data.push({ x: r.roundNumber, y: r.roundScore })
+          );
+          return (
+            <LineSeries
+              key={p._id}
+              color={lineColors[index]}
+              strokeWidth={4}
+              data={data}
+            />
+          );
         })}
 
-        <XAxis title="round" />
-        <YAxis title="score" />
+        <XAxis title="round" position="middle" />
+        <YAxis title="score" position="middle" />
       </XYPlot>
+
+      <div></div>
     </Paper>
   );
 };
@@ -35,10 +67,8 @@ const Chart = ({ players, rounds, maxNumberOfRounds }) => {
 const mapStateToProps = state => ({
   players: state.game.players,
   rounds: state.game.rounds,
-  maxNumberOfRounds: state.game.maxNumberOfRounds,
+  currentRound: state.game.currentRound,
+  maxNumberOfRounds: state.game.maxNumberOfRounds
 });
 
-export default connect(
-  mapStateToProps,
-  null
-)(Chart);
+export default connect(mapStateToProps, null)(Chart);
